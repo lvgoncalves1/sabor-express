bc14041a6f4cf0a76f2e00ecca663079e226584f
Lucas Verissimo <lucasverissimo1799@gmail.com>
2026-07-06 20:23:52 -0300

    Add evaluations for Restaurante da Praça
    Added functionality to receive evaluations for the restaurant.

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ from model.restaurante import Restaurante
 
 restaurante_praca = Restaurante("Restaurante da Praça", "Brasileira")
 
+restaurante_praca.receber_avaliacao("João", 5)
+restaurante_praca.receber_avaliacao("Maria", 4)
+restaurante_praca.receber_avaliacao("Pedro", 3)
 
 def main():
     Restaurante.listar_restaurantes()
