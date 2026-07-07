0a7fe767bfdefee8475651cd37650d55b03f3de3
lucas <lucasver1799@gmail.com>
2026-07-07 18:52:27 -0300

    Adiciona menu prato e bebida

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from model.restaurante import Restaurante
+from model.menu.prato import Prato
+from model.menu.bebida import Bebida
+
 
 restaurante_praca = Restaurante("Restaurante da Praça", "Brasileira")
-
+bebida_suco = Bebida("Suco de Maça", 10.0, "Grande")
+prato_pizza = Prato("Pizza", 25.0, "Pizza de Pepperoni")
 
 def main():
-    Restaurante.listar_restaurantes()
+    pass
 
 if __name__ == "__main__":
     main()
